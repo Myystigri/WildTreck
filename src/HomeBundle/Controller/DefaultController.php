9c4b9ea405998944b2fe6046cd07a801ef31aa9e
Nico <nicolascapron403@gmail.com>
2016-05-12 19:10:35 +0200

    parameters

--- src/HomeBundle/Controller/DefaultController.php
+++ src/HomeBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('HomeBundle:Home:home.html.twig');
+        return $this->render('HomeBundle:home:home.html.twig');
     }
 }
