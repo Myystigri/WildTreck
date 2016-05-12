983f0753064995413169b345d7bf1c2766683415
keny700 <fredericbouillere@yahoo.fr>
2016-05-12 21:33:31 +0200

    page vue

--- src/HomeBundle/Controller/DefaultController.php
+++ src/HomeBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('HomeBundle:Home:home.html.twig');
+        return $this->render('HomeBundle:home:home.html.twig');
     }
+
+
+    public function vueAction()
+    {
+        return $this->render('HomeBundle:vue:vue.html.twig');
+    }
+
 }
